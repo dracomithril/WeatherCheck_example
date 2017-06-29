091658276054a935f399ada695dbbf20ecce2058
Draco Mithril <dracomithril@gmail.com>
2017-06-30 01:27:45 +0200

    first commit

--- proxy_lb/index.js
+++ proxy_lb/index.js
@@ -4,9 +4,19 @@
 const restyfy = require('restify');
 
 
-const server =restyfy.createServer({});
+const server = restyfy.createServer({});
 
-server.get('/', function (req, res, next) {
+
+server.get('/random', function (req, res, next) {
+    const random =Math.floor(Math.random() * 100);
+    console.log(random);
+    res.send(random.toString());
+    return next();
+});
+
+server.get('/getWeather', function (req, res, next) {
+    //https://www.metaweather.com/api/location/44418/
+
     res.send('hello world');
     return next();
 });
