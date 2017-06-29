c2c42813336af215fb7fa6ac8046384648766220
Draco Mithril <dracomithril@gmail.com>
2017-06-29 18:29:23 +0200

    first commit

---
+++ endpoint_control/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Gryzli on 29.06.2017.
+ */
+const restyfy = require('restify');
+
+
+const server =restyfy.createServer({});
+
+server.get('/', function (req, res, next) {
+    res.send('hello world');
+    return next();
+});
+
+server.listen(8080);
